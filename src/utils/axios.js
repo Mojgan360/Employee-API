d6b99d8dac21a261a116fb54820c2feeb4ba2ed1
Mojgan360 <mojgan360@gmail.com>
2022-05-22 16:11:42 +0200

    env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
-// const { REACT_APP_AUTH_KEY } = process.env
+const { REACT_APP_AUTH_KEY } = process.env
 const instance = axios.create({
-  // baseURL: '  http://localhost:5000',
-
   baseURL: 'https://api.1337co.de/v3',
   headers: {
-    Authorization:
-      'api-key 14:2022-05-15:lucas.stenberg@1337.tech 882d55f88078823d8df53bf0144fb4a6f9f664ae0fe9a48f1613b6a38f709f37',
+    Authorization: REACT_APP_AUTH_KEY,
   },
 })
 
